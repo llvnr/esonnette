fe595983863e413e0e6993387852e9bfaba5b24c
Yuchiima <134304602+Yuchiima@users.noreply.github.com>
2023-07-05 13:20:31 +0400

    FRONTEND - V1.0.0.0 - Création de la page inscription full html

---
+++ resources/views/inscription.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inscription - Votre sonnette virtuel connecté</title>
+    <link rel="stylesheet" href="{{ asset("css/style.css") }}">
+    <link rel="stylesheet" href="{{ asset("css/responsive.css") }}">
+</head>
+<body>
+    
+    <div class="Shellregister">
+
+        <div class="Shellregister__title">ESONNETTE</div>
+
+        <input type="text" class="Shellregister__name" placeholder="Votre nom...">
+
+        <input type="text" class="Shellregister__surname" placeholder="Votre prénom...">
+
+        <input type="email" class="Shellregister__email" placeholder="Votre adresse email...">
+
+        <input type="password" class="Shellregister__password" placeholder="Mot de passe...">
+
+        <button class="Shellregister__btnregister">Inscription</button>
+
+    </div>
+
+</body>
+</html>
