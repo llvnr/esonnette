b229885174a26494583e1153580012248e792ca5
Yuchiima <134304602+Yuchiima@users.noreply.github.com>
2023-07-05 11:02:53 +0400

    FRONTEND - V1.0.0.0 - Création de la page login full HTML

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Connexion - Votre sonnette virtuel connecté</title>
+    <link rel="stylesheet" href="{{ asset("css/style.css") }}">
+    <link rel="stylesheet" href="{{ asset("css/responsive.css") }}">
+</head>
+<body>
+    
+    <div class="Shellogin">
+
+        <div class="Shellogin__title">ESONNETTE</div>
+
+        <input type="email" class="Shellogin__email" placeholder="Votre adresse email...">
+
+        <input type="password" class="Shellogin__password" placeholder="Mot de passe...">
+
+        <button class="Shellogin__btnlogin">Connexion</button>
+
+    </div>
+
+</body>
+</html>
