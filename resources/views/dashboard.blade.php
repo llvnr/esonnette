4259c427a18e7c02b04c2fe031e783ef2a8b33a2
llvnr <ludovic.lvnr.freelance@gmail.com>
2023-08-24 03:41:36 +0400

    FRONTEND - V100 - Ajout des cards statistiques sur la page dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,6 +23,7 @@
                 <ul class="SidebarContent__navigation">
                     <a href="/" class="noDecor"><li class="SidebarContent__navigation-element">Dashboard</li></a>
                     <a href="/propriete" class="noDecor"><li class="SidebarContent__navigation-element">Mes propriétés</li></a>
+                    <a href="/parametres" class="noDecor"><li class="SidebarContent__navigation-element">Paramètres</li></a>
                     <a href="/" class="noDecor"><li class="SidebarContent__navigation-element">Déconnexion</li></a>
                 </ul>
 
@@ -50,7 +51,76 @@
                 </div>
 
             </div>
-            <div class="ShellDashboard__content-body">TEST DASHBOARD</div>
+            <div class="ShellDashboard__content-body">
+
+                <div class="ContentBody__row">
+
+                    <div class="ContentBody__row-card">
+                        <div class="ContentBody__row-card-title">Content de vous revoir !</div>
+                        <div class="ContentBody__row-card-infos">
+                            Ici vous pouvez voir toutes les données importantes en un seul endroit !
+                        </div>
+                    </div>
+
+                    <div class="ContentBody__row-card">
+                        <div class="ContentBody__row-card-counter">10</div>
+                        <div class="ContentBody__row-card-title">Utilisateurs</div>
+                        <div class="ContentBody__row-card-infos">
+                            Comptabilise le nombre d'utilisateurs qui gère cette instance.
+                        </div>
+                    </div>
+
+                    <div class="ContentBody__row-card">
+                        <div class="ContentBody__row-card-counter">5</div>
+                        <div class="ContentBody__row-card-title">Propriétés</div>
+                        <div class="ContentBody__row-card-infos">
+                            Comptabilise le nombre de propriétés associé à cette instance.
+                        </div>
+                    </div>
+
+                    <div class="ContentBody__row-card">
+                        <div class="ContentBody__row-card-counter">15</div>
+                        <div class="ContentBody__row-card-title">Visite(s) totale(s)</div>
+                        <div class="ContentBody__row-card-infos">
+                            Comptabilise le nombre de visite reçu sur l'ensemble des propriétés.
+                        </div>
+                    </div>
+
+                    <div class="ContentBody__row-card">
+                        <div class="ContentBody__row-card-counter">25</div>
+                        <div class="ContentBody__row-card-title">Visite(s) manqué(s)</div>
+                        <div class="ContentBody__row-card-infos">
+                            Comptabilise le nombre de visite manqué sur l'ensemble des propriétés.
+                        </div>
+                    </div>
+
+                    <div class="ContentBody__row-card">
+                        <div class="ContentBody__row-card-counter">35</div>
+                        <div class="ContentBody__row-card-title">Alerte(s) email(s)</div>
+                        <div class="ContentBody__row-card-infos">
+                            Comptabilise le nombre d'alertes par email envoyées sur l'ensemble des propriétés.
+                        </div>
+                    </div>
+
+                    <div class="ContentBody__row-card">
+                        <div class="ContentBody__row-card-counter">45</div>
+                        <div class="ContentBody__row-card-title">Alerte(s) discord</div>
+                        <div class="ContentBody__row-card-infos">
+                            Comptabilise le nombre d'alertes discord envoyés sur l'ensemble des propriétés.
+                        </div>
+                    </div>
+
+                    <div class="ContentBody__row-card">
+                        <div class="ContentBody__row-card-counter">55</div>
+                        <div class="ContentBody__row-card-title">Alerte(s) SMS</div>
+                        <div class="ContentBody__row-card-infos">
+                            Comptabilise le nombre d'alertes SMS envoyés sur l'ensemble des propriétés.
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
 
