b990f9cc74adb16d2e89b8acbe9ee217f5213e74
llvnr <ludovic.lvnr.freelance@gmail.com>
2023-08-29 11:55:54 +0400

    Finalisation de la vue visualisation propriété

--- resources/js/views/Voirpropriete.vue
+++ resources/js/views/Voirpropriete.vue
@@ -14,39 +14,57 @@
                 <div class="ContentBodyVoirPropriete__content">
                     <div class="ContentBodyVoirPropriete__content-colonneUn">
 
-                        <div class="ContentColonneUn__label-image">
-                            <img src="https://img.icons8.com/?size=100&id=np6r5nc6uNNj&format=png" alt="">
+                        <div class="ContentColonneUn__Card">
+                            <div class="ContentColonneUn__label-image">
+                                <img src="https://img.icons8.com/?size=100&id=np6r5nc6uNNj&format=png" alt="">
+                            </div>
+                            <div class="ContentColonneUn__label-content">
+                                <div class="ContentColonneUn__label-fullname">Ludovic LEVENEUR</div>
+                                <div class="ContentColonneUn__label-adresse">56 bis chemin du ruisseau</div>
+                                <div class="ContentColonneUn__label-codepostal">97421</div>
+                                <div class="ContentColonneUn__label-ville">La rivière</div>
+                            </div>
                         </div>
-                        <div class="ContentColonneUn__label-content">
-                            <div class="ContentColonneUn__label-fullname">Ludovic LEVENEUR</div>
-                            <div class="ContentColonneUn__label-adresse">56 bis chemin du ruisseau</div>
-                            <div class="ContentColonneUn__label-codepostal">97421</div>
-                            <div class="ContentColonneUn__label-ville">La rivière</div>
-                        </div>
+
+                        <button class="ContentColonneUn__btn-enableqr">Activer le QrCode</button>
 
                     </div>
                     <div class="ContentBodyVoirPropriete__content-colonneDeux">
 
-                        <div class="ContentColonneDeux__label-visite-total"><b>3</b> Visite(s) totale(s)</div>
-                        <div class="ContentColonneDeux__label-visite-manquer"><b>2</b> Visite(s) manqué</div>
-                        <button class="ContentColonneDeux__btn-enableqr">Activer le QrCode</button>
+                        <div class="ContentColonneDeux__Card">
+
+                            <div class="ContentColonneDeux__label-image">
+                                <img src="https://img.icons8.com/?size=100&id=13019&format=png" alt="">
+                            </div>
+                            <div class="ContentColonneDeux__label-content">
+                                <div class="ContentColonneDeux__label-visite-total"><b>3</b> Visite(s) totale(s)</div>
+                                <div class="ContentColonneDeux__label-visite-manquer"><b>2</b> Visite(s) manqué</div>
+                                <div class="ContentColonneDeux__label-qrcode">Etat du QRCODE : <span class="ContentColonneDeux__label-etat-qrcode state-activate">Activé</span></div> 
+                                
+                            </div>
+
+                        </div>
+
                         <button class="ContentColonneDeux__btn-disableqr">Désactiver le QrCode</button>
 
                     </div>
                 </div>
 
+                <div class="ContentBodyDataVisite">
+                    
+                    <div v-for="n in 10" class="cardVisite">
 
-                <!-- {{-- <div class="ContentBodyADDPropriete__label-name">Nom du propriétaire</div>
+                        <div class="cardVisite__label-id">1</div>
+                        <div class="cardVisite__label-image"><img src="https://img.icons8.com/?size=40&id=Hjmjz1QamXpz&format=png" alt=""></div>
+                        <div class="cardVisite__label-date">29/08/2023</div>
+                        <div class="cardVisite__label-heure">11:37</div>
+                        <div class="cardVisite__label-name">Nom ou nom d'entreprise</div>
+                        <div class="cardVisite__label-type">Type : Email / Discord / Sms</div>
+                        <div class="cardVisite__label-status">Statut</div>
 
-                <div class="ContentBodyADDPropriete__label-surname">Prénom du propriétaire</div>
+                    </div>
 
-                <div class="ContentBodyADDPropriete__label-type">Type de propriété</div>
-                
-                <div class="ContentBodyADDPropriete__label-adresse">Adresse de la propriété</div>
-
-                <div class="ContentBodyADDPropriete__label-codepostal">Code postal</div>
-
-                <div class="ContentBodyADDPropriete__label-ville">Ville</div> --}} -->
+                </div>
 
             </div>
 
