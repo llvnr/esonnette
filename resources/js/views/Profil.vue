a319fe93a1f2df5f256845176adb07288b5e79f1
llvnr <ludovic.lvnr.freelance@gmail.com>
2023-08-29 10:19:40 +0400

    Finalisation de la vue profil

--- resources/js/views/Profil.vue
+++ resources/js/views/Profil.vue
@@ -13,18 +13,26 @@
 
                 <div class="ContentBody__colonneUn">
 
-                    <div class="ContentBody__colonneUn-label-username">Nom d'utilisateur *</div>
-                    <input type="text" class="ContentBody__colonneUn-input-username" placeholder="Nom d'utilisateur">
+                    <div class="ContentBody__colonneUn-card">
 
-                    <button class="ContentBody__colonneUn-btnmdf">Modifier</button>
+                        <div class="ContentBody__colonneUn-label-username">Nom d'utilisateur *</div>
+                        <input type="text" class="ContentBody__colonneUn-input-username" placeholder="Nom d'utilisateur">
+
+                        <div class="ContentBody__colonneUn-label-email">Adresse email * <small class="ContentBody__colonneDeux-confirmation account-status-danger">Non confirmé</small></div>
+                        <input type="email" class="ContentBody__colonneUn-input-email" placeholder="Votre adresse email">
+
+                        <button class="ContentBody__colonneUn-btnmdf">Modifier</button>
+
+                    </div>
+
+                    <div class="ContentBody__colonneUn-card">
+                        <button class="ContentBody__colonneUn-btndelete">Supprimer mon compte</button>
+                    </div>
 
                 </div>
                 <div class="ContentBody__colonneDeux">
 
-                    <div class="ContentBody__colonneDeux-label-email">Adresse email * <small class="ContentBody__colonneDeux-confirmation account-status-success">Non confirmé</small></div>
-                    <input type="email" class="ContentBody__colonneDeux-input-email" placeholder="Votre adresse email">
 
-                    <button class="ContentBody__colonneDeux-btnmdf">Modifier</button>
 
                 </div>
 
