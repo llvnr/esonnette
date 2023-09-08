9cc36fd7eef7316e3d6826cfc0d4c685199633c8
llvnr <ludovic.lvnr.freelance@gmail.com>
2023-09-08 17:43:14 +0400

    Création du controller AlerteController et de la fonction create.

---
+++ app/Http/Controllers/AlerteController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+use App\Models\Alerte;
+
+class AlerteController extends Controller
+{
+    //
+
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+    */
+    public function __construct() {
+        $this->middleware('auth:api');
+    }
+
+    /**
+     * Create Alerte
+     *
+     * @return void
+    */
+
+    public function create(Request $request){
+
+        try {
+            //code...
+
+            $validator = Validator::make($request->all(), [
+                'id' => 'required',
+                'type' => 'required',
+                'data' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
+            }
+
+            $id = $request->id;
+            $type = $request->type;
+            $data = $request->data;
+
+            $checkAlerte = Alerte::where('propriete_id', $id)->where('type', $type)->exists();
+
+            if($checkAlerte){
+                return response()->json([
+                    "status" => false,
+                    "message" => "Une alerte [".$type."] existe déjà.";
+                ], 500);
+            } else {
+
+                $create = Alerte::create([
+                    "propriete_id" => $id,
+                    "type" => $type,
+                    "informations" => $data,
+                    "etat" => 1
+                ]);
+    
+                return response()->json([
+                    "status" => true,
+                    "result" => $create
+                ]);
+
+            }
+
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                "status" => false,
+                "message" => $th->getMessage()
+            ], 500);
+        }
+
+    }
+
+}
