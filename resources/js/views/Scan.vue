803f4f9718e57ef955bef187a290f91d696a58ce
llvnr <ludovic.lvnr.freelance@gmail.com>
2023-09-13 16:53:26 +0400

    Mise en place de la partie front de la page de scan.

--- resources/js/views/Scan.vue
+++ resources/js/views/Scan.vue
@@ -1,3 +1,27 @@
 <template>
-    <div>SCAN</div>
+    <div class="ShellScan">
+        <div class="ShellScan__Header">Bienvenue chez</div>
+        <div class="ShellScan__Body">
+            <div class="ShellScan__Body-Title">Ludovic LEVENEUR</div>
+            <div class="ShellScan__Body-Content">
+
+                <div class="ShellScan__Body-Content-label">Qui êtes vous ?</div>
+                <input type="text" class="ShellBody__Body-Content-input-name" placeholder="Votre nom ou entreprise" />
+                <input type="text" class="ShellBody__Body-Content-input-telephone" placeholder="Votre numéro de téléphone" />
+                <div class="ShellScan__Body-Content-infos">
+                    Comment voulez vous sonner ?
+                </div>
+                <select class="ShellScan__Body-Content-Canaux">
+                    <option value="Email">Via Email</option>
+                    <option value="Discord">Via Discord</option>
+                </select>
+                <div class="ShellScan__Body-Content-infos">
+                    Besoin d'une sonnette comme ça chez vous ? <br> Rendez-vous sur 
+                    <a href="https://yakelkun.llvnr.com" target="_blank">https://yakelkun.llvnr.com</a>
+                </div>
+
+            </div>
+        </div>
+        <div class="ShellScan__Footer">SONNER</div>
+    </div>
 </template>
