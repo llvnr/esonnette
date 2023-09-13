494f600d20e4981daf067b8bfe965456a1c59a00
llvnr <ludovic.lvnr.freelance@gmail.com>
2023-09-13 18:42:32 +0400

    Correction effet CSS sur texte de la page scan.

--- resources/js/views/Scan.vue
+++ resources/js/views/Scan.vue
@@ -6,22 +6,41 @@
             <div class="ShellScan__Body-Content">
 
                 <div class="ShellScan__Body-Content-label">Qui êtes vous ?</div>
-                <input type="text" class="ShellBody__Body-Content-input-name" placeholder="Votre nom ou entreprise" />
-                <input type="text" class="ShellBody__Body-Content-input-telephone" placeholder="Votre numéro de téléphone" />
+                <input type="text" class="ShellBody__Body-Content-input-name" v-model="denomination" placeholder="Votre nom ou entreprise" />
+                <input type="text" class="ShellBody__Body-Content-input-telephone" v-model="telephone" placeholder="Votre numéro de téléphone" />
                 <div class="ShellScan__Body-Content-infos">
                     Comment voulez vous sonner ?
                 </div>
-                <select class="ShellScan__Body-Content-Canaux">
+                <select class="ShellScan__Body-Content-Canaux" v-model="canaux">
                     <option value="Email">Via Email</option>
                     <option value="Discord">Via Discord</option>
                 </select>
-                <div class="ShellScan__Body-Content-infos">
+                <div class="ShellScan__Body-Content-infospub">
                     Besoin d'une sonnette comme ça chez vous ? <br> Rendez-vous sur 
                     <a href="https://yakelkun.llvnr.com" target="_blank">https://yakelkun.llvnr.com</a>
                 </div>
 
             </div>
         </div>
-        <div class="ShellScan__Footer">SONNER</div>
+        <div class="ShellScan__Footer" @click="this.dringdring">SONNER</div>
     </div>
 </template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            denomination: '',
+            telephone: '',
+            canaux: 'Email'
+        }
+    },
+    methods: {
+        dringdring() {
+            alert('dring dring !')
+        }
+    }
+}
+
+</script>
