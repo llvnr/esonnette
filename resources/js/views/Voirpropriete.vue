1245cf399d332bb62d9814f4a2924826f78037cb
llvnr <ludovic.lvnr.freelance@gmail.com>
2023-09-13 14:50:13 +0400

    Réadaptation de la page voirpropriete

--- resources/js/views/Voirpropriete.vue
+++ resources/js/views/Voirpropriete.vue
@@ -1,81 +1,87 @@
 <template>
 
-    <div class="ShellDashboard__content">
+    <div class="ShellDashboard">
 
-        <div class="ShellDashboard__content-header">
+        <Sidebar :isLogging="true" />
 
-            <Header title="Fiche d'informations" />
+        <div class="ShellDashboard__content">
 
-        </div>
-        <div class="ShellDashboard__content-body">
+            <div class="ShellDashboard__content-header">
 
-            <div class="ContentBodyVoirPropriete">
+                <Header title="Fiche d'informations" />
 
-                <div class="ContentBodyVoirPropriete__content">
-                    <div class="ContentBodyVoirPropriete__content-colonneUn">
+            </div>
+            <div class="ShellDashboard__content-body">
 
-                        <div class="ContentColonneUn__Card">
-                            <div class="ContentColonneUn__label-image">
-                                <img v-if="infoPropriete.typepropriete === 'Maison'" src="https://img.icons8.com/?size=100&id=np6r5nc6uNNj&format=png" alt="">
-                                <img v-else-if="infoPropriete.typepropriete === 'Appartement'" src="https://img.icons8.com/?size=100&id=FrBSkVzw5GE_&format=png" />
+                <div class="ContentBodyVoirPropriete">
+
+                    <div class="ContentBodyVoirPropriete__content">
+                        <div class="ContentBodyVoirPropriete__content-colonneUn">
+
+                            <div class="ContentColonneUn__Card">
+                                <div class="ContentColonneUn__label-image">
+                                    <img v-if="infoPropriete.typepropriete === 'Maison'" src="https://img.icons8.com/?size=100&id=np6r5nc6uNNj&format=png" alt="">
+                                    <img v-else-if="infoPropriete.typepropriete === 'Appartement'" src="https://img.icons8.com/?size=100&id=FrBSkVzw5GE_&format=png" />
+                                </div>
+                                <div class="ContentColonneUn__label-content">
+                                    <div class="ContentColonneUn__label-fullname">{{ infoPropriete.prenom + ' ' + infoPropriete.nom }}</div>
+                                    <div class="ContentColonneUn__label-adresse">{{ infoPropriete.adresse }}</div>
+                                    <div class="ContentColonneUn__label-codepostal">{{ infoPropriete.codepostal  }}</div>
+                                    <div class="ContentColonneUn__label-ville">{{ infoPropriete.ville }}</div>
+                                </div>
                             </div>
-                            <div class="ContentColonneUn__label-content">
-                                <div class="ContentColonneUn__label-fullname">{{ infoPropriete.prenom + ' ' + infoPropriete.nom }}</div>
-                                <div class="ContentColonneUn__label-adresse">{{ infoPropriete.adresse }}</div>
-                                <div class="ContentColonneUn__label-codepostal">{{ infoPropriete.codepostal  }}</div>
-                                <div class="ContentColonneUn__label-ville">{{ infoPropriete.ville }}</div>
-                            </div>
+
+                            <button v-if="infoPropriete.status" class="ContentColonneUn__btn-disableqr" @click="changeStateQrcode(0)">Désactiver le QrCode</button>
+                            <button v-else class="ContentColonneUn__btn-enableqr" @click="changeStateQrcode(1)">Activer le QrCode</button>
+
                         </div>
+                        <div class="ContentBodyVoirPropriete__content-colonneDeux">
 
-                        <button v-if="infoPropriete.status" class="ContentColonneUn__btn-disableqr" @click="changeStateQrcode(0)">Désactiver le QrCode</button>
-                        <button v-else class="ContentColonneUn__btn-enableqr" @click="changeStateQrcode(1)">Activer le QrCode</button>
+                            <div class="ContentColonneDeux__Card">
 
+                                <div class="ContentColonneDeux__label-image">
+                                    <router-link :to="'/propriete/qrcode/' + getID">
+                                        <img :src="infoPropriete.qrcode" width="100" alt="">
+                                    </router-link>
+                                </div>
+                                <div class="ContentColonneDeux__label-content">
+                                    <div class="ContentColonneDeux__label-visite-total"><b>0</b> Visite(s) totale(s)</div>
+                                    <div class="ContentColonneDeux__label-visite-manquer"><b>0</b> Visite(s) manqué</div>
+                                    <div class="ContentColonneDeux__label-qrcode">Etat du QRCODE : 
+                                        <span v-if="infoPropriete.status" class="ContentColonneDeux__label-etat-qrcode state-activate">Activé</span>
+                                        <span v-else class="ContentColonneDeux__label-etat-qrcode state-desactivate">Désactivé</span>
+                                    </div> 
+                                    
+                                </div>
+
+                            </div>
+
+                            <router-link :to="'/alerte/show/one/' + getID">
+                                <button class="ContentColonneDeux__btn-alerte">Gérer les alertes</button>
+                            </router-link>
+
+                        </div>
                     </div>
-                    <div class="ContentBodyVoirPropriete__content-colonneDeux">
 
-                        <div class="ContentColonneDeux__Card">
+                    <div class="ContentBodyDataVisite">
+                        
+                        <div v-for="(item, index) in allVisite" class="cardVisite">
 
-                            <div class="ContentColonneDeux__label-image">
-                                <router-link :to="'/propriete/qrcode/' + getID">
-                                    <img :src="infoPropriete.qrcode" width="100" alt="">
-                                </router-link>
+                            <div class="cardVisite__label-id">{{ item.id }}</div>
+                            <div class="cardVisite__label-image"><img src="https://img.icons8.com/?size=40&id=Hjmjz1QamXpz&format=png" alt=""></div>
+                            <div class="cardVisite__label-date">{{ formatDate(item.created_at) }}</div>
+                            <div class="cardVisite__label-name">{{ item.denomination }}</div>
+                            <div class="cardVisite__label-contact">{{ item.telephone }}</div>
+                            <div v-if="item.alerte.type === 'email'" class="cardVisite__label-type">
+                                <img src="https://img.icons8.com/?size=25&id=D9x0PpvvT1AL&format=png" />
                             </div>
-                            <div class="ContentColonneDeux__label-content">
-                                <div class="ContentColonneDeux__label-visite-total"><b>0</b> Visite(s) totale(s)</div>
-                                <div class="ContentColonneDeux__label-visite-manquer"><b>0</b> Visite(s) manqué</div>
-                                <div class="ContentColonneDeux__label-qrcode">Etat du QRCODE : 
-                                    <span v-if="infoPropriete.status" class="ContentColonneDeux__label-etat-qrcode state-activate">Activé</span>
-                                    <span v-else class="ContentColonneDeux__label-etat-qrcode state-desactivate">Désactivé</span>
-                                </div> 
-                                
+                            <div v-else-if="item.alerte.type === 'discord'" class="cardVisite__label-type">
+                                <img src="https://img.icons8.com/?size=25&id=30998&format=png" />
+                            </div>
+                            <div class="cardVisite__label-status" :class="item.etat ? 'state-activate-write' : 'state-desactivate-write'">
+                                {{ item.etat ? 'Vu' : 'Manqué' }}
                             </div>
 
-                        </div>
-
-                        <router-link :to="'/alerte/show/one/' + getID">
-                            <button class="ContentColonneDeux__btn-alerte">Gérer les alertes</button>
-                        </router-link>
-
-                    </div>
-                </div>
-
-                <div class="ContentBodyDataVisite">
-                    
-                    <div v-for="(item, index) in allVisite" class="cardVisite">
-
-                        <div class="cardVisite__label-id">{{ item.id }}</div>
-                        <div class="cardVisite__label-image"><img src="https://img.icons8.com/?size=40&id=Hjmjz1QamXpz&format=png" alt=""></div>
-                        <div class="cardVisite__label-date">{{ formatDate(item.created_at) }}</div>
-                        <div class="cardVisite__label-name">{{ item.denomination }}</div>
-                        <div class="cardVisite__label-contact">{{ item.telephone }}</div>
-                        <div v-if="item.alerte.type === 'email'" class="cardVisite__label-type">
-                            <img src="https://img.icons8.com/?size=25&id=D9x0PpvvT1AL&format=png" />
-                        </div>
-                        <div v-else-if="item.alerte.type === 'discord'" class="cardVisite__label-type">
-                            <img src="https://img.icons8.com/?size=25&id=30998&format=png" />
-                        </div>
-                        <div class="cardVisite__label-status" :class="item.etat ? 'state-activate-write' : 'state-desactivate-write'">
-                            {{ item.etat ? 'Vu' : 'Manqué' }}
                         </div>
 
                     </div>
@@ -92,6 +98,7 @@
 
 <script>
 
+import Sidebar from '../components/Sidebar.vue';
 import Header from '../components/Header.vue';
 
 export default {
@@ -221,7 +228,7 @@ export default {
 
         }
     },
-    components: { Header }
+    components: { Sidebar, Header }
 
 }
 
