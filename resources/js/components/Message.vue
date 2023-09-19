2130ad7a1c724780065a971dbb5b54399010cae2
llvnr <ludovic.lvnr.freelance@gmail.com>
2023-09-19 10:01:02 +0400

    Création du prototype du composant Message.

---
+++ resources/js/components/Message.vue
@@ -0,0 +1,61 @@
+<template>
+    <div v-if="type === 'success'" class="alert alert-success">
+        Message !
+    </div>
+    <div v-else-if="type === 'danger'" class="alert alert-danger">
+        Message !
+    </div>
+    <div v-else-if="type === 'warning'" class="alert alert-warning">
+        Message !
+    </div>
+    <div v-else-if="type === 'info'" class="alert alert-info">
+        Message !
+    </div>
+</template>
+
+<style>
+
+.alert {
+    width: 100%;
+    height: 2em;
+    max-height: 2em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.alert-success {
+    border-left: 5px solid #79AE79;
+    background-color: #A8CD5D;
+    color: white;
+}
+
+.alert-info {
+    border-left: 5px solid #076D91;
+    background-color: #3397DB;
+    color: white;
+}
+
+.alert-danger {
+    border-left: 5px solid #EA9496;
+    background-color: #F79E9F;
+    color: white;
+}
+
+.alert-warning {
+    border-left: 5px solid #DD6137;
+    background-color: #F7931D;
+    color: white;
+}
+
+</style>
+
+<script>
+
+export default {
+    props: {
+        type: ''
+    }
+}
+
+</script>
