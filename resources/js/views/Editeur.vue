89cbe099d19e56f4870fc4bd6410ed7af296cc0a
llvnr <ludovic.lvnr.freelance@gmail.com>
2023-09-21 08:33:34 +0400

    Intégration du layout sur la page de l'éditeur de sticker.

--- resources/js/views/Editeur.vue
+++ resources/js/views/Editeur.vue
@@ -1,3 +1,43 @@
 <template>
-    EDITEUR
+
+    <div v-if="!loadData" class="ShellLoader">
+        <Loader />
+    </div>
+    <div v-else class="ShellDashboard">
+
+        <Sidebar />
+
+        <div class="ShellDashboard__content">
+
+            <div class="ShellDashboard__content-header">
+
+                <Header title="Editeur de Sticker" />
+
+            </div>
+            <div class="ShellDashboard__content-body">
+
+                EDITEUR
+
+            </div>
+
+        </div>
+    </div>
+
 </template>
+
+<script>
+
+import Loader from '../components/Loader.vue';
+import Header from '../components/Header.vue';
+import Sidebar from '../components/Sidebar.vue';
+
+export default {
+    data() {
+        return {
+            loadData: true
+        }
+    },
+    components: { Loader, Header, Sidebar }
+}
+
+</script>
