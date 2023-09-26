445750c49e50c3f0df5ee639d06053e720e086bf
llvnr <ludovic.lvnr@gmail.com>
2023-09-26 13:17:57 +0400

    Changement du titre de l'application.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Yakelkun - Votre sonnette virtuelle</title>
+    <title>ESonnette - Votre sonnette virtuelle</title>
     <link rel="stylesheet" href="{{ asset("css/style.css") }}">
     <link rel="stylesheet" href="{{ asset("css/responsive.css") }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
